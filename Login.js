9a6450dd2823291254cedc94fe43ebd2f4291b78
DeveshDighe <deveshdighe30@gmail.com>
2023-12-09 22:03:28 +0530

    b

--- Login.js
+++ Login.js
@@ -2,14 +2,12 @@ function LoginEvent(event) {
 
     event.preventDefault();
         
-      let name = document.getElementById("Name").value;
-      console.log(name , 'name');
       let email = document.getElementById("Email").value;
       console.log(email);
       let passWord = document.getElementById("Password").value;
       console.log(passWord);
     
-      if (!name || !email || !passWord) {
+      if (!email || !passWord) {
         return alert("Please fill inputs");
       }
     
@@ -19,10 +17,16 @@ function LoginEvent(event) {
             if(users[i].email == email && users[i].passWord == passWord){
 
                 document.getElementById("Email").value = '';
-                document.getElementById("Email").value = '';
+                document.getElementById("Password").value = '';
+
+                window.location.href = './main.html'
 
                 return alert ('login sucessfull')
+                
+                
+              
 
             }
         }
     }
+    
